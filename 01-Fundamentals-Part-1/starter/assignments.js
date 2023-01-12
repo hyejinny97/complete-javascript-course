50f54f4a41b6cd333b3e49c4895fa512342f60eb
hyejinny97 <yhj970102@gmail.com>
2023-01-12 16:10:13 +0900

    17.Template Literals ~ 20.Type Conversion/Coercion

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -43,3 +43,34 @@ console.log(population < 33);
 // 5. Based on the variables you created, create a new variable 'description' which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'
 let description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
 console.log(description)
+
+
+// ----- LECTURE: Strings and Template Literals -----
+// 1. Recreate the 'description' variable from the last assignment, this time using the template literal syntax
+description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description);
+
+
+// ----- LECTURE: Taking Decisions: if / else Statements -----
+// 1. If your country's population is greater that 33 million, log a string like this to the console: 'Portugal's population is above average'. Otherwise, log a string like 'Portugal's population is 22 million below average' (the 22 is the average of 33 minus the country's population)
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(`${country}'s population is ${33 - population} million below average`);
+}
+// 2. After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original
+
+
+// ----- LECTURE: Type Conversion and Coercion -----
+// 1. Predict the result of these 5 operations without executing them:
+// '9' - '5' => 4
+// '19' - '13' + '17' => '617'
+// '19' - '13' + 17 => 23
+// '123' < 57 => false
+// 5 + 6 + '4' + 9 - 4 - 2 => 1143
+// 2. Execute the operations to check if you were right
+console.log('9' - '5')
+console.log('19' - '13' + '17')
+console.log('19' - '13' + 17)
+console.log('123' < 57)
+console.log(5 + 6 + '4' + 9 - 4 - 2)
--- 01-Fundamentals-Part-1/starter/challenges.js
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -11,3 +11,12 @@ console.log(BMIMark, BMIJohn);
 // 3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
 const markHigherBMI = BMIMark > BMIJohn;
 console.log(markHigherBMI);
+
+
+// ----- Coding Challenge #2 -----
+// Use the BMI example from Challenge #1, and the code you already wrote, and improve it.
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+}
