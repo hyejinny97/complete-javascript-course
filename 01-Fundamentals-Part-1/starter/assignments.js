f795da2edae258393c2bbb2ec00f363574908511
hyejinny97 <yhj970102@gmail.com>
2023-01-14 16:58:07 +0900

    21.Truthy/Falsy Values ~ 25.Coding Challenge 3

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -74,3 +74,60 @@ console.log('19' - '13' + '17')
 console.log('19' - '13' + 17)
 console.log('123' < 57)
 console.log(5 + 6 + '4' + 9 - 4 - 2)
+
+
+// ----- LECTURE: : Equality Operators: == vs. === -----
+/*
+// 1. Declare a variable 'numNeighbours' based on a prompt input like this: prompt('How many neighbour countries does your country have?');
+const numNeighbours = prompt('How many neighbour countries does your country have?');
+// 2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality == for now)
+if (numNeighbours == 1) {
+  console.log('Only 1 border!');
+}
+// 3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours' is greater than 1
+if (numNeighbours == 1) {
+  console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+  console.log('More than 1 border');
+}
+// 4. Use an else block to log 'No borders' (this block will be executed when 'numNeighbours' is 0 or any other value)
+if (numNeighbours == 1) {
+  console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+  console.log('More than 1 border');
+} else {
+  console.log('No borders');
+}
+// 5. Test the code with different values of 'numNeighbours', including 1 and 0.
+// 6. Change == to ===, and test the code again, with the same values of 'numNeighbours'. Notice what happens when there is exactly 1 border! Why is this happening?
+if (numNeighbours === 1) {
+  console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+  console.log('More than 1 border');
+} else {
+  console.log('No borders');  // => 1을 입력했을때 'No borders'가 출력됨
+}
+// 7. Finally, convert 'numNeighbours' to a number, and watch what happens now when you input 1
+if (Number(numNeighbours) === 1) {
+  console.log('Only 1 border!');  // => 1을 입력했을때 'Only 1 border!'가 출력됨
+} else if (Number(numNeighbours) > 1) {
+  console.log('More than 1 border');
+} else {
+  console.log('No borders');
+}
+// 8. Reflect on why we should use the === operator and type conversion in this situation
+*/
+
+
+// ----- LECTURE: Logical Operators -----
+// 1. Comment out the previous code so the prompt doesn't get in the way
+// 2. Let's say Sarah is looking for a new country to live in. She wants to live in a country that speaks english, has less than 50 million people and is not an island.
+// 3. Write an if statement to help Sarah figure out if your country is right for her. You will need to write a condition that accounts for all of Sarah's criteria. Take your time with this, and check part of the solution if necessary.
+const sarahCriteria = language === 'english' && population < 50 && !isIsland
+// 4. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If not, log 'Portugal does not meet your criteria :('
+if (sarahCriteria) {
+  console.log(`You should live in ${country} :)`);
+} else {
+  console.log(`${country} does not meet your criteria :(`);
+}
+// 5. Probably your country does not meet all the criteria. So go back and temporarily change some variables in order to make the condition true (unless you live in Canada :D)
--- 01-Fundamentals-Part-1/starter/challenges.js
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -20,3 +20,44 @@ if (BMIMark > BMIJohn) {
 } else {
   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
 }
+
+
+// ----- Coding Challenge #3 -----
+// 1. Calculate the average score for each team, using the test data below
+let dolphin = (96 + 108 + 89) / 3
+let koalas = (88 + 91 + 110) / 3
+console.log(dolphin, koalas)
+// 2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score)
+if (dolphin === koalas) {
+  console.log('draw')
+} else if (dolphin > koalas) {
+  console.log('dolphin win!')
+} else {
+  console.log('koalas win!')
+}
+// 3. Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. 
+dolphin = (97 + 112 + 101) / 3
+koalas = (109 + 95 + 123) / 3
+console.log(dolphin, koalas)
+if (dolphin === koalas) {
+  console.log('draw')
+} else if (dolphin > koalas && dolphin >= 100) {
+  console.log('dolphin win!')
+} else if (dolphin < koalas && koalas >= 100) {
+  console.log('koalas win!')
+} else {
+  console.log('no team wins the trophy')
+}
+// 4. Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+dolphin = (97 + 112 + 101) / 3
+koalas = (109 + 95 + 106) / 3
+console.log(dolphin, koalas)
+if (dolphin === koalas && dolphin >= 100 && koalas >= 100) {
+  console.log('draw')
+} else if (dolphin > koalas && dolphin >= 100) {
+  console.log('dolphin win!')
+} else if (dolphin < koalas && koalas >= 100) {
+  console.log('koalas win!')
+} else {
+  console.log('no team wins the trophy')
+}
